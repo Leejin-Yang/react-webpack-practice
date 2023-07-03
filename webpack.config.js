432f65bd232e8fc54b893e024a7833a97ca2edb6
Leejin-Yang <ejin1996@gmail.com>
2023-07-03 22:40:05 +0900

    chore: set react, webpack, typescript

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    bundle: './src/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js', // name은 entry의 프로퍼티 키값을 따른다
+    clean: true, // dist내 이전 파일 삭제
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'ts-loader' }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      minify:
+        process.env.NODE_ENV === 'production'
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true, // 주석 제거
+            }
+          : false,
+      hash: true, // 정적 파일을 불러올때 쿼리문자열에 웹팩 해쉬값을 추가
+    }),
+  ],
+};
